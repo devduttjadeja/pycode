0219f70b3cd65c9115e6f940d74f3bf13d434f27
Devdutt <devduttjadeja12345@gmail.com>
2020-06-22 22:11:41 -0400

    new files in web scrapping

---
+++ Medium/links.py
@@ -0,0 +1,7 @@
+# scrapping flights from kayak
+# https://towardsdatascience.com/if-you-like-to-travel-let-python-help-you-scrape-the-best-fares-5a1f26213086
+# https://www.ca.kayak.com/flights/YMQ-AMD/2020-12-25-flexible?sort=price_a
+
+# https://towardsdatascience.com/what-i-learnt-from-taking-a-masters-in-computer-vision-and-machine-learning-69f0c6dfe9df
+
+# https://towardsdatascience.com/real-time-and-video-processing-object-detection-using-tensorflow-opencv-and-docker-2be1694726e5
---
+++ WebScrapping/Scapping_Flights_Prices_Kayak.py
@@ -0,0 +1,2 @@
+# https://towardsdatascience.com/if-you-like-to-travel-let-python-help-you-scrape-the-best-fares-5a1f26213086
+# link https://www.ca.kayak.com/flights/YUL-AMD/2020-12-25-flexible?sort=price_a
